52313f02ea0ae21e18896dbdad73173973a5252b
screwax <screwaxx@gmail.com>
2023-08-02 16:25:31 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const k1 = "5px";
+const k2 = "12djd334";
+const k3 = "12.45asdwe12";
+const k4 = "qwqweeewq";
+
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+
+console.log(Number.parseFloat(k1));
+console.log(Number.parseFloat(k2));
+console.log(Number.parseFloat(k3));
+console.log(Number.parseFloat(k4));
+
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+const random = Math.random() * (19 - 3) + 1;
+console.log(Number.parseInt(random));
+
+const number = "11";
+const of = Math.sqrt(number);
+console.log(of);
+
+const testA = 8;
+const testB = "2";
+const testC = parseInt(testB);
+console.log(testC);
+const testF = toString(testA);
+console.log(testF);
